16b5a5fee0ecced235f131d04e8cf698e2e929b1
povmaniaco <devnull@localhost>
2012-09-07 23:16:16 +0200

    Continue divided project Place more code into header files

--- include/luxsi_material.h
+++ include/luxsi_material.h
@@ -2,7 +2,8 @@
 LuXSI - Autodesk(c) Softimage(c) XSI Export addon for the LuxRender  Renderer
 (http://www.luxrender.org)
 
-Copyright (C) 2010 2011 2012  Michael Gangolf and Pedro Alcaide
+Copyright (C) 2010 - 2012  Michael Gangolf 
+Code contributor ; Pedro Alcaide, aka povmaniaco
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,35 +19,87 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+
+
+
+#ifndef LUXSI_MATERIAL_H
+#define LUXSI_MATERIAL_H
+
 #include "luxsi_main.h"
 
+
+//using namespace MATH;
 using namespace std;
 using namespace XSI;
-//using namespace MATH;
 
 extern float red, green, blue, alpha;
-extern CString shaderType;
 extern Application app;
-extern CStringArray aMatList;
+extern  CStringArray aMatList;
 extern ofstream f;
 extern float vContrast;
 //--
-extern bool luxsi_find(CStringArray a, CString s);
+extern bool luxsi_find(CStringArray in_array, CString in_string);
+//-
 extern string replace(string input);
+//-
+extern bool luxdebug;
+
+//- declare local variables
+float b_red, b_green, b_blue, b_alpha;
+
+float sp_red, sp_green, sp_blue, sp_alpha;
+
+float refl_red, refl_green, refl_blue, refl_alpha;
+
+float mRough;
+
+//ImageClip2 vBumpFile;
+bool vIsSet=false;
+
+Material mat;
+Texture vTexture;
+
+//- generic string containers for shader definitions
+CString 
+	shaderStr,		//- general definitions
+	vChanel,		//- chanel texture type
+	shaderType;		//- shader type
+
+
+/* Find texture connect to shader port */
+/**/
+Texture find_tex_used(Shader in_shader, CString in_string);
+
+/* Find an specific shader port */
+/**/
+CString find_shader_used( Shader in_shader, CString in_string);
+
+/* Write LuxRender Glass shader */
+/**/
+CString write_lux_glass(Shader in_shader, CString in_string, CString in_ID);
+
+/* Write LuxRender Metal shader */
+/**/
+CString write_lux_metal(Shader in_shader, CString in_string);
+
+/* Write LuxRender Car Paint shader */
+/**/
+CString write_lux_car_paint(Shader in_shader, CString in_string);
+
+/* Write test to translate an generic mia_material_phen shader */
+/**/
+CString write_mia_material_phen(Shader in_shader, CString in_string);
+
+/* Write LuxRender Shinymetal shader (deprecated?)*/
+/**/
+CString write_lux_shinymetal(Shader in_shader, CString in_string);
 
 //--
-CString write_lux_glass(Shader s, CString shStr);
-//--
-CString write_lux_roughglass(Shader s, CString shStr);
-//--
-CString write_lux_metal(Shader s, CString shStr);
-//--
-CString write_lux_car_paint(Shader s, CString shStr);
-//--
-CString write_mia_material_phen(Shader s, CString shStr);
-//--
-CString write_lux_shinymetal(Shader s, CString shStr);
-//--
-CString write_lux_substrate( Shader s, CString shStr);
-//--
-CString write_lux_mattetranslucent(Shader s, CString shStr);
+CString write_lux_substrate(Shader in_shader, CString in_string);
+
+/* Write LuxRender Matte and Matte Translucent shader */
+/**/
+CString write_lux_matte(Shader in_shader, CString in_string);
+
+
+#endif //LUXSI_MATERIAL_H
