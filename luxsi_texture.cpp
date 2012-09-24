3d1bbd1516d2e54aeecb882f67dc290a8fe8cb5f
povmaniaco <devnull@localhost>
2012-09-24 02:53:02 +0200

    Refactory texture function and prepare for expand

--- luxsi_texture.cpp
+++ luxsi_texture.cpp
@@ -27,119 +27,65 @@ using namespace std;
 
 //-
 extern Application app;
-extern ofstream f;
+//-
 extern float vContrast;
+//-
 extern CString vChanel;
-extern bool luxdebug;
-
-extern CStringArray aMatList;
 //-
 extern string replace(string input);
 //-
-extern bool luxsi_find(CStringArray in_array, CString in_string);
 
-//-
-void luxsi_texture()
+CString luxsi_texture(Material mat, Shader s, CString texStr)
 {
     //--
-    Scene scene = app.GetActiveProject().GetActiveScene();
-    Library matlib = scene.GetActiveMaterialLibrary();
+    CRefArray texMat = s.GetShaders();
 
-    CRefArray materials = matlib.GetItems();
-    //- ( 1 ) for materials..
-    for ( LONG i=0; i < materials.GetCount(); i++ )
+    //-( 3 ) for textures..
+    for (int k=0; k < texMat.GetCount(); k++)
     {
-        Texture vTexture;
-        CString vChanType=L"";
-
-        Shader vBumpTex;
+        Texture tex = texMat[k];
+        CString vTexID(tex.GetProgID().Split(L".")[1]);
+        // test
         CString texFact=L"";
-        bool vNorm=false;
-
-        //---------------------------
-        Material mat( materials[i] );
-        //---------------------------
-        if ( int(mat.GetUsedBy().GetCount())== 0 ) continue;
-
-        CString MatName(mat.GetName());
-
-        if ( !luxsi_find(aMatList, MatName ) ) aMatList.Add(MatName);
-        else continue;
-        //--
-        CRefArray shad(mat.GetShaders()); // Array of all shaders attached to the material [e.g. phong]
-
-        //- ( 2 ) for shaders..
-        for ( long j = 0; j < shad.GetCount(); j++)
+        //bool vNorm=false;
+        CString vChanType=L"color";
+              
+        if (vTexID == L"txt2d-image-explicit" || vTexID == L"Softimage.txt2d-image-explicit.1")
         {
-            Shader s(shad[j]); // j en vez de 0 ?
-            //---
-            CString vMatID((s.GetProgID()).Split(L".")[1]);
-            CRefArray texMat = s.GetShaders();
+            //--
+            ImageClip2 vImgClip(tex.GetImageClip() );
+            Source vImgClipSrc(vImgClip.GetSource());
+            CString vFileName = vImgClipSrc.GetParameterValue( L"path");
 
-            //-( 3 ) for textures..
-            for (int k=0; k < texMat.GetCount(); k++)
+            //- test for search 'mapping' type -----------------------------
+            //CString textproj  = vTexture.GetParameterValue(L"tspace_id");
+            //app.LogMessage(CString(textproj.GetAsciiString()));
+            //--------------------------------------------------------------
+
+            //- TODO; synch by shader
+            CString _tex_name = mat.GetName() + L"_"+ tex.GetName();
+                 
+            //- 
+            if ( bool(tex.GetParameterValue(L"bump_inuse")) == true && vChanel == L"bumpmap")
             {
-                vTexture = texMat[k];
-                CString vWhat(vTexture.GetProgID().Split(L".")[1]);
-                //-
-                if (vWhat==L"txt2d-image-explicit" || vWhat==L"Softimage.txt2d-image-explicit.1")
-                {
-                    //--
-                    ImageClip2 vImgClip(vTexture.GetImageClip() );
-                    Source vImgClipSrc(vImgClip.GetSource());
-                    CString vFileName = vImgClipSrc.GetParameterValue( L"path");
-
-                    //- test for search 'mapping' type -----------------------------
-                    //CString textproj  = vTexture.GetParameterValue(L"tspace_id");
-                    //app.LogMessage(CString(textproj.GetAsciiString()));
-                    //--------------------------------------------------------------
-
-                    //- TODO; synch by shader
-                    CString _tex_name = mat.GetName() + L"_"+ vTexture.GetName();
-
-                    //- old test for bump chanel
-                    if ((bool)vTexture.GetParameterValue(L"bump_inuse")!=false)
-                    {
-                        vNorm = true;
-                        vChanel = L"bumpmap";
-                        vChanType = L"float";
-                        texFact = vTexture.GetParameterValue(L"factor");
-                    }
-                    else
-                    {
-                        //-
-                        vChanType = L"color";
-                    }
-                    //----/ rewrite all /-------->
-                    f << "\nTexture \"" << _tex_name.GetAsciiString() <<"\" \""<< vChanType.GetAsciiString() <<"\" \"imagemap\" \n";
-                    f << "  \"string wrap\" [\"repeat\"] \n";//TODO; create option at spdl shader
-                    //f << "    \"string chanel\" [\"mean\"] \n"; // not work??
-                    f << "  \"string filename\" [\"" << replace(vFileName.GetAsciiString()) << "\"] \n";
-                    f << "  \"float gamma\" ["<< vContrast <<"]\n";
-                    f << "  \"float gain\" [1.000000]\n";
-                    f << "  \"string filtertype\" [\"bilinear\"] \n";// TODO; create option
-                    f << "  \"string mapping\" [\"uv\"] \n";// TODO; create option / search data in XSI
-                    f << "  \"float vscale\" [-1.0]\n";
-                    f << "  \"float uscale\" [1.0] \n";
-                    f << "  \"float udelta\" [0.000000] \n";
-                    f << "  \"float vdelta\" [1.000000] \n";
-                    //-
-                    vNorm = false;
-                }
+                //--
+                vChanType = L"float";
             }
-            /*
-            f.write('\n' +
-            'Texture "%s" "color" "imagemap"\n' % tex_name +
-            '    "string filename" ["%s"]\n' % _file_name +
-            '    "float gamma" [2.2000]\n' +
-            '    "string mapping" ["spherical"]\n' +
-            '    "float udelta" [0.0000]\n' +
-            '    #"vector v1" [1.000 0.000 0.000]\n' +
-            '    #"vector v2" [0.000 1.000 0.000]\n' +
-            '    "float vdelta" [0.0000]\n'
-            )
-            f.close()
-            */
+                    
+            //----/ rewrite all /-------->
+            texStr += L"\nTexture \"" + _tex_name + L"\" \""+ vChanType + L"\" \"imagemap\" \n";
+            texStr += L"  \"string wrap\" [\"repeat\"] \n";//TODO; create option at spdl shader
+            //f << "    \"string chanel\" [\"mean\"] \n"; // not work??
+            texStr += L"  \"string filename\" [\""+ CString(replace(vFileName.GetAsciiString()).c_str()) + L"\"] \n";
+            texStr += L"  \"float gamma\" ["+ CString( vContrast ) + L"]\n";
+            texStr += L"  \"float gain\" [1.000000]\n";
+            texStr += L"  \"string filtertype\" [\"bilinear\"] \n";// TODO; create option
+            texStr += L"  \"string mapping\" [\"uv\"] \n";// TODO; create option / search data in XSI
+            texStr += L"  \"float vscale\" [-1.0]\n";
+            texStr += L"  \"float uscale\" [1.0] \n";
+            texStr += L"  \"float udelta\" [0.000000] \n";
+            texStr += L"  \"float vdelta\" [1.000000] \n";                    
         }
     }
+    return texStr;
 }
